8f3a4e8a14076f05a728ddcbc5e64fe823d9ed88
Adonay <54407072+F70443969@users.noreply.github.com>
2022-03-24 21:59:40 -0600

    0tes

---
+++ humantalent/src/main/java/com/group12/humantalent/HumantalentApplication.java
@@ -0,0 +1,13 @@
+package com.group12.humantalent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumantalentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumantalentApplication.class, args);
+	}
+
+}
---
+++ humantalent/src/test/java/com/group12/humantalent/HumantalentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group12.humantalent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumantalentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
